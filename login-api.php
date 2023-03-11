98a153a0cf7cc1a55bd1ada71d22cc0dc9bbd521
Sana Rayess <rayess.sana@gmail.com>
2023-03-11 12:50:12 +0200

    created login api

---
+++ login-api.php
@@ -0,0 +1,45 @@
+
+<?php
+// establish database connection
+$servername = "localhost";
+$username = "root";
+$password = NULL;
+$dbname = "healthcaredb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+  
+    $sql = "SELECT u.type, t.type as user_type FROM users u INNER JOIN user_types t ON u.type = t.id WHERE email='$email' AND password='$password'";
+    $result = $conn->query($sql);
+  
+    // if the user exists, set session variables and redirect to appropriate page based on user type
+    if ($result->num_rows == 1) {
+      $row = $result->fetch_assoc();
+      session_start();
+      $_SESSION["user_type"] = $row["user_type"];
+      $_SESSION["user_id"] = $row["id"];
+      if ($row["user_type"] == "patient") {
+        header("Location: patient.html");
+        exit();
+      } elseif ($row["user_type"] == "employee") {
+        header("Location: employee.html");
+        exit();
+      } elseif ($row["user_type"] == "admin") {
+        header("Location: admin.html");
+        exit();
+      }
+    } else {
+      
+      $_SESSION['error_message'] = "Invalid email or password";
+      header("Location: login.html");
+      exit();
+    }
+  }
+ 
+?>
+
