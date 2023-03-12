c201d29f8d262be94ac3034fc5a26a94587fa7d7
Sana Rayess <rayess.sana@gmail.com>
2023-03-12 02:05:00 +0200

    added js to connect to signup api

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+
 function login() {
     // retrieve form data
     var email = document.getElementById("email").value;
@@ -9,3 +10,44 @@ function login() {
     // submit form
     document.getElementById("login-form").submit();
   }
+  
+  
+  const form = document.getElementById('signup-form');
+  const responseMessage = document.createElement('p');
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const name = form.elements['name'].value;
+      const email = form.elements['email'].value;
+      const password = form.elements['password'].value;
+      const dob = form.elements['dob'].value;
+      const type = form.elements['type'].value;
+
+      const data = new FormData();
+      data.append('name', name);
+      data.append('email', email);
+      data.append('password', password);
+      data.append('dob', dob);
+      data.append('type', type);
+
+      fetch('signup-api.php', {
+          method: 'POST',
+          body: data
+      })
+      .then(response => response.json())
+      .then(data => {
+          responseMessage.textContent = data.message;
+          if(data.status === 'success') {
+              responseMessage.style.color = 'green';
+          } else {
+              responseMessage.style.color = 'red';
+          }
+          form.appendChild(responseMessage);
+      })
+      .catch(error => {
+          responseMessage.textContent = 'An error occurred. Please try again.';
+          responseMessage.style.color = 'red';
+          form.appendChild(responseMessage);
+      });
+  });
