47b04d4835d77940e1ef8993cdcd7bb26715abf9
Sana Rayess <rayess.sana@gmail.com>
2023-03-12 11:20:38 +0200

    added a function to add employees to employee_nfos table

--- admin.js
+++ admin.js
@@ -93,6 +93,7 @@ function getUsers() {
       isActive;
     xhr.send(params);
   }
+
   function getEmployees() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'get-employees.php');
@@ -111,4 +112,40 @@ function getUsers() {
       }
     };
     xhr.send();
+  }
+
+  function assignEmployee() {
+    const userId = document.getElementById("user-dropdown").value;
+    const hospitalId = document.getElementById("hospital-dropdown").value;
+    const dateJoined = document.getElementById("date-joined").value;
+    const dateLeft = document.getElementById("date-left").value;
+    const ssn = document.getElementById("ssn").value;
+    const position = document.getElementById("position").value;
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "assign-employee.php");
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        const message = document.getElementById("message");
+        message.innerHTML = response.message;
+      } else {
+        console.log("Error: " + xhr.status);
+      }
+    };
+    const params =
+      "user_id=" +
+      userId +
+      "&hospital_id=" +
+      hospitalId +
+      "&date_joined=" +
+      dateJoined +
+      "&date_left=" +
+      dateLeft +
+      "&ssn=" +
+      ssn +
+      "&position=" +
+      position;
+    xhr.send(params);
   }
