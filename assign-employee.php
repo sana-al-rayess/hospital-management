59094cbbffa15270a98617d627e9be3cdfc9c392
Sana Rayess <rayess.sana@gmail.com>
2023-03-12 10:44:30 +0200

    created api to add employees to employe_infos table

---
+++ assign-employee.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = NULL;
+$dbname = "healthcaredb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$user_id = $_POST["user_id"];
+$hospital_id = $_POST["hospital_id"];
+$ssn = $_POST["ssn"];
+$date_left = $_POST["date_left"];
+$date_joined = $_POST["date_joined"];
+$position = $_POST["position"];
+
+// $sql = "INSERT INTO hospital_users (user_id, hospital_id, date_joined, date_left, is_active) VALUES ('$user_id', '$hospital_id', '$date_joined', '$date_left', '$is_active')";
+$sql="INSERT INTO employee_infos (user_id, hospital_id, ssn, position, date_joined, date_left) VALUES ('$user_id', '$hospital_id', '$ssn', '$position', '$date_joined', '$date_left')";
+
+
+if ($conn->query($sql) === TRUE) {
+  $response = ["success" => true, "message" => "New record created successfully"];
+  echo json_encode($response);
+} else {
+  $response = ["success" => false, "message" => "Error: " . $sql . "<br>" . $conn->error];
+  echo json_encode($response);
+}
+
+$conn->close();
+?>
