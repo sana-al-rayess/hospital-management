1cce37409752fa8afaecbaa745caa2d49294f24e
Sana Rayess <rayess.sana@gmail.com>
2023-03-12 02:07:38 +0200

    added api to view list of hospital in a ddl

---
+++ hospital-list.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = NULL;
+$dbname = "healthcaredb";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name FROM hospitals";
+$result = $conn->query($sql);
+
+$hospitals = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $hospital = array("id" => $row["id"], "name" => $row["name"]);
+    array_push($hospitals, $hospital);
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+echo json_encode($hospitals);
+?>
+
+  
+  
+  
+  
