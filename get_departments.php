2e14afb3ccbb8ae7338dc3f41bee0279a2e6c683
Sana Rayess <rayess.sana@gmail.com>
2023-03-12 02:11:08 +0200

    created api to view departments of specific hospital in a ddl

---
+++ get_departments.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = NULL;
+$dbname = "healthcaredb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$hospital_id = $_GET['hospital_id'];
+
+$sql = "SELECT id, name FROM departments WHERE hospital_id=$hospital_id";
+$result = $conn->query($sql);
+
+$departments = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $department = array("id" => $row["id"], "name" => $row["name"]);
+    array_push($departments, $department);
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+echo json_encode($departments);
+?>
